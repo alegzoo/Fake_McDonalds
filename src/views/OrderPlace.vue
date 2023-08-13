c794bf191b52566e3b2733f471b11cc00dc30716
alegzoo <alexaliascasillas@gmail.com>
2023-08-13 15:40:34 +0200

    edit orderplace

--- src/views/OrderPlace.vue
+++ src/views/OrderPlace.vue
@@ -1,5 +1,16 @@
 <template>
     <v-container fluid class="h-100 w-100">
+        <v-row class="h-100">
+            <v-col cols="2"/>
+            <v-col cols="6">
+
+            </v-col>
+            <v-col cols="6">
+
+            </v-col>
+            <v-col></v-col>
+        </v-row>
+
     </v-container>
 
 
